490a44b173e5990d7062bc2e6e675adc4190f886
angg07 <anggasyamsul70@Gmail.com>
2022-06-11 09:40:00 +0700

    COmmit on 11 Juni 2022

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+let logoSoang = document.getElementById("logo-soang")
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 ||document.documentElement.scrollTop > 20) 
+    {
+        logoSoang.style.width = "60px";
+    } else {
+        logoSoang.style.width = "80px";
+    }
+}
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+
+
+  
